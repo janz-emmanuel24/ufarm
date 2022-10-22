30e8ed368fd391ef7da8926205b336d856d3c4c6
janz-emmanuel24 <jansabbas1@gmail.com>
2022-10-22 23:53:41 +0300

    started route implementation

--- app.js
+++ app.js
@@ -3,35 +3,74 @@ const path = require('path');
 //dependencies
 const express = require('express');
 const app = express()
+const mongoose = require('mongoose')
+const configurations = require('./config/config')
 
-//routes
-const agricOfficerRoutes = require('./routes/agricOfficerRoute');
-const farmerOneRoutes = require('./routes/farmerOneRoute')
-const urbanFarmerRoutes = require('./routes/urbanFarmerRoute')
-const publicUserRoutes = require('./routes/publicUserRoute')
+//requireModels
+const AgricOfficerModel = require('./model/AgricOfficerModel');
 
 //pug config
 app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views/agricOfficer'), path.join(__dirname, 'views/farmerOne'),  path.join(__dirname, 'views/BoxSelections')])
 
 //middleware
 // app.use('/public/uploads', express.static(__dirname + '/public/uploads')) 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+// app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(`${__dirname}/public`))
 
-//Routes middleware
-app.use('/agricOfficer', agricOfficerRoutes);
-app.use('/farmerOne', farmerOneRoutes)
-app.use('/urbanFarmer', urbanFarmerRoutes)
-app.use('/publicUser', publicUserRoutes)
-
-
+/** DATABASE CONNECTIONS */
+mongoose.connect(`${configurations.DB}`, {useNewUrlParser: true})
+    .then(() => console.log('Connected to Database'))
+    .catch(err => console.log(err))
 
 //routes
 app.get('/', (req,res) => {
     res.status(200).render('landingpage')
 })
+
+app.get('/agricOfficer', async (req, res) => {
+    const registeredFarmerOnes = await AgricOfficerModel.find()
+    // console.log(registeredFarmerOnes)
+    res.render('agricOfficerDashboard', {registeredFarmerOnes: registeredFarmerOnes})
+    // res.send(registeredFarmerOnes);
+})
+
+app.post('/registerFarmerOnes', async (req,res) => {
+    const farmerOneRegistration = new AgricOfficerModel(req.body)
+    // farmerOneRegistration.activities = ['register', 'approve']
+    res.send(farmerOneRegistration)
+    await farmerOneRegistration.save();
+})
+
+app.get('/updateFarmerOne/:farmerOneId', async (req, res) => {
+    const farmerOneId = req.params.farmerOneId;
+    const farmerOne = await AgricOfficerModel.findOne({_id: farmerOneId})
+    // console.log(farmerOne)
+    res.render('agricOfficerUpdateFarmerOne', {farmerOne: farmerOne})
+})
+
+app.post('/updateFarmerOne/:farmerOneId', async (req, res) => {
+    const farmerOneId = req.params.farmerOneId;
+    await AgricOfficerModel.findByIdAndUpdate(farmerOneId, req.body)
+    res.redirect('/agricOfficer')
+})
+
+app.get('/farmerOne', (req, res) => {
+    res.render('farmerOneDashboard')
+})
+
+app.get('/categoriesInShop', (req, res) => {
+    res.render('categoriesInShop')
+})
+
+app.get('/categoriesInShop/vegetables', (req, res) => {
+    res.render('vegetables')
+})
+
+
 
 const port = 4000 || process.env.PORT;
 
